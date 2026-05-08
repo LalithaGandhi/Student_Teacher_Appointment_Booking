5eb9d18dfa5a2c6d3a520faaeba5d039f30da71f
LalithaGandhi <lalithagandhi2001@gmail.com>
2026-05-08 13:41:07 +0530

    Update teacherRoutes.js

--- backend/routes/teacherRoutes.js
+++ backend/routes/teacherRoutes.js
@@ -9,10 +9,7 @@ router.post('/login', login)
 router.patch('/updatePassword', verifyToken, updatePassword)
 
 router.route('/schedule').get(verifyToken, allow('admin', 'teacher'), getAllAppointments).post(verifyToken, allow('admin', 'teacher'), createAppointment)
-
 router.route('/reschedule/:id').delete(verifyToken,allow('teacher'),deleteAppointment);
-
 router.route('/changeApprovalStatus/:id/:studentId').delete(verifyToken, allow('admin', 'teacher'), dissapproveAppointment).patch(verifyToken,allow('admin','teacher'),approveAppointment)
-
 router.route('/getAllPendingStudents').get(verifyToken,allow('teacher'),getAllPendingStudents);
 module.exports = router
