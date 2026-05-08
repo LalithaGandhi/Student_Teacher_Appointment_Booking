6700dc0ea609734886bd3c69e18eca9de42dcfb4
LalithaGandhi <lalithagandhi2001@gmail.com>
2026-05-08 13:40:25 +0530

    Update studentRoutes.js

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -15,6 +15,4 @@ router.route('/login').post(login)
 router.route('/appointment/:id').patch(verifyToken, allow('student'), bookAppointment)
 router.route('/appointment/getTeachersWithAppointments').get(verifyToken, allow('student'), getTeacherWithAppointments)
 router.route('/appointment/getRegisteredAppointments').get(verifyToken, allow('student'), registeredAppointments)
-
-
 module.exports = router
