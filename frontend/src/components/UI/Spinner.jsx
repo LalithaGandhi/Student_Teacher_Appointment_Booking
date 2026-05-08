655cab95d9a05650123798e182cc26178b605f69
LalithaGandhi <lalithagandhi2001@gmail.com>
2026-05-08 14:37:06 +0530

    Update Spinner.jsx

--- frontend/src/components/UI/Spinner.jsx
+++ frontend/src/components/UI/Spinner.jsx
@@ -3,12 +3,8 @@ import './spinner.css'
 function Spinner() {
     return (
         <div className="flex h-screen  justify-center items-center dark:bg-slate-800">
-
             <span className="loader dark:border-white"></span>
-
         </div>
-
     )
 }
-
 export default Spinner
