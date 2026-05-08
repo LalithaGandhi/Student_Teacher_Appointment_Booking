e2a5d50938327278a8e5d381784cb85d42a5f8ac
LalithaGandhi <lalithagandhi2001@gmail.com>
2026-05-08 14:21:52 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "./context/ThemeContext";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <ThemeProvider>
